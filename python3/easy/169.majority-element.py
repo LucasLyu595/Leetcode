3f25e5cdd288e5a6abb7e28f6cef1d77f862bf13
Xinlou Lyu <lvxl595@gmail.com>
2023-03-16 04:16:19 -0400

    feat(169): python second attempt

--- python3/easy/169.majority-element.py
+++ python3/easy/169.majority-element.py
@@ -7,16 +7,8 @@
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        length = len(nums)
-        target = length // 2
-        seem = {}
-        for i in nums:
-            if i not in seem:
-                seem[i] = 1
-            else :
-                seem[i] += 1
-            if seem[i] > target:
-                return i
+        nums.sort()
+        return nums[len(nums)//2]
         
 # @lc code=end
 
