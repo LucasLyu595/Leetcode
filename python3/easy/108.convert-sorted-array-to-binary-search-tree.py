1c4da72924bc94fa28f8cc91a5241069b2898012
Xinlou Lyu <lvxl595@gmail.com>
2023-07-10 14:34:13 -0400

    feat(108): python first attempt, recursion

---
+++ python3/easy/108.convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=108 lang=python3
+#
+# [108] Convert Sorted Array to Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        l = len(nums)
+        if not l:
+            return None
+        
+        mid = l // 2
+        return TreeNode(
+            nums[mid],
+            self.sortedArrayToBST(nums[:mid]), self.sortedArrayToBST(nums[mid + 1:])
+        )
+        
+# @lc code=end
+
