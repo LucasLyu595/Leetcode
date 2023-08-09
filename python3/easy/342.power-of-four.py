234d499328a794ce96432c7655670cf0784f6aa3
Xinlou Lyu <lvxl595@gmail.com>
2023-08-09 17:10:35 -0400

    feat(342): python second attempt, sqrt()

--- python3/easy/342.power-of-four.py
+++ python3/easy/342.power-of-four.py
@@ -7,7 +7,7 @@
 # @lc code=start
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        return n > 0 and not(n &(n - 1)) and n & 1431655765 == n
+        return n > 0 and not(n &(n - 1)) and int(sqrt(n)) * int(sqrt(n)) == n
         
 # @lc code=end
 
