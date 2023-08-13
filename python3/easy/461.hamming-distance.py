0c5cdc34a96b72c6e218181c05671306ddecbc09
Xinlou Lyu <lvxl595@gmail.com>
2023-08-12 20:09:50 -0400

    feat(461): python third attempt, Brian Kernighan's Algorithm

--- python3/easy/461.hamming-distance.py
+++ python3/easy/461.hamming-distance.py
@@ -10,10 +10,9 @@ class Solution:
         xor = x ^ y
         distance = 0
         while xor:
-            # mask out the rest bits
-            if xor & 1:
-                distance += 1
-            xor = xor >> 1
+            distance += 1
+            # remove the rightmost bit of '1'
+            xor = xor & (xor - 1)
         return distance
 
         
