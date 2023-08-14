49d1f8f0dc3f3adadfb6c428f46a70b7c5e22f8d
Xinlou Lyu <lvxl595@gmail.com>
2023-08-14 01:36:56 -0400

    feat(67): python second attempt, bit manipulation

--- python3/easy/67.add-binary.py
+++ python3/easy/67.add-binary.py
@@ -7,7 +7,10 @@
 # @lc code=start
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return '{0:b}'.format(int(a, 2) + int(b, 2))
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            x, y = x ^ y, (x & y) << 1
+        return bin(x)[2:] 
         
 # @lc code=end
 
