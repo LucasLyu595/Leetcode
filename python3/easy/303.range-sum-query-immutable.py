c13a1976a5f4cd481d37e44de28f548a1f5faa0c
Xinlou Lyu <lvxl595@gmail.com>
2023-08-14 22:54:21 -0400

    feat(303): python first attempt, memorization

---
+++ python3/easy/303.range-sum-query-immutable.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=303 lang=python3
+#
+# [303] Range Sum Query - Immutable
+#
+
+# @lc code=start
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.sum = []
+        tmp = 0
+        for i in nums:
+            tmp += i
+            self.sum.append(tmp)
+        
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left > 0 and right > 0:
+            return self.sum[right] - self.sum[left - 1]
+        else:
+            return self.sum[left or right]
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
+# @lc code=end
+
