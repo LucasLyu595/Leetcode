179d421e81703a7cf3f17ea962cb4e8b9137d10a
Xinlou Lyu <lvxl595@gmail.com>
2023-08-21 22:28:35 -0400

    feat(231): python second attempt, Get the Rightmost 1-bit

--- python3/easy/231.power-of-two.py
+++ python3/easy/231.power-of-two.py
@@ -7,7 +7,7 @@
 # @lc code=start
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and n & (n-1) == 0
+        return n > 0 and n & (-n) == n
         
 # @lc code=end
 
