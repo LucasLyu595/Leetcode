aeff6dc2f467e27f071d614bf708ad0196c4e769
Xinlou Lyu <lvxl595@gmail.com>
2023-08-28 12:25:51 -0400

    feat(205): python third attempt, First occurence transformation

--- python3/easy/205.isomorphic-strings.py
+++ python3/easy/205.isomorphic-strings.py
@@ -6,9 +6,19 @@
 
 # @lc code=start
 class Solution:
+    def transform(self, string: str) -> str:
+        i = 0
+        res = []
+        dic = {}
+        for letter in string:
+            if letter not in dic:
+                dic[letter] = str(i)
+                i += 1
+            res.append(dic[letter])
+        return ' '.join(res)
+    
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return len(set(s)) == len(set(t)) == len(set(zip(s, t)))
-
+        return self.transform(s) == self.transform(t)
 
         
 # @lc code=end
