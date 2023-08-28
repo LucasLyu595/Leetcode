506c83a56bbc54d05b0dc80a386c9b2483c8e64b
Xinlou Lyu <lvxl595@gmail.com>
2023-08-28 12:05:29 -0400

    feat(392): python second attempt, hash map

--- python3/easy/392.is-subsequence.py
+++ python3/easy/392.is-subsequence.py
@@ -7,18 +7,19 @@
 # @lc code=start
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        if len(s) == 0:
-            return True
-        if len(t) == 0:
-            return False
-        i = j = 0
-        while j < len(t):
-            if s[i] == t[j]:
-                i += 1
-                if i == len(s):
-                    return True
-            j += 1
-        return False
-        
+        dic = defaultdict(list)
+        for i, letter in enumerate(t):
+            dic[letter].append(i)
+        cur = -1
+        for letter in s:
+            if letter not in dic:
+                return False
+            l = dic[letter]
+            i = bisect.bisect_right(l, cur)
+            if i != len(l):
+                cur = l[i]
+            else:
+                return False
+        return True
 # @lc code=end
 
