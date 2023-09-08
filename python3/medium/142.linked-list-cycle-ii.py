4d092e3d5263caa9ccf0f2adc54982e5648fbc60
Xinlou Lyu <lvxl595@gmail.com>
2023-09-08 13:46:51 -0400

    feat(142): python first attempt, two pointers

---
+++ python3/medium/142.linked-list-cycle-ii.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=142 lang=python3
+#
+# [142] Linked List Cycle II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                break
+        else: 
+            return None
+        while head != slow:
+            head, slow = head.next, slow.next
+        return head
+        
+# @lc code=end
+
