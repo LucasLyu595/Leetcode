68f8ca54e1d59093031f1ed96bd07c1a965d1ea2
Xinlou Lyu <lvxl595@gmail.com>
2024-01-16 15:02:16 -0500

    feat(200), python second attempt, bfs

--- python3/medium/200.number-of-islands.py
+++ python3/medium/200.number-of-islands.py
@@ -6,24 +6,24 @@
 
 # @lc code=start
 class Solution: 
-    def dfs(self, grid: List[List[str]], i: int, j: int) -> None:
-        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != "1":
-            return
-        grid[i][j] = "0"
-        self.dfs(grid, i, j+1)
-        self.dfs(grid, i, j-1)
-        self.dfs(grid, i+1, j)
-        self.dfs(grid, i-1, j)
-
     def numIslands(self, grid: List[List[str]]) -> int:
         res = 0
         if not grid:
             return res
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
-                    self.dfs(grid, i, j)
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] == "1":
                     res += 1
+                    neighbors = [(x, y)]
+                    while neighbors:
+                        (i, j) = neighbors.pop(0)
+                        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != "1":
+                            continue
+                        grid[i][j] = "0"
+                        neighbors.append((i, j-1))
+                        neighbors.append((i, j+1))
+                        neighbors.append((i-1, j))
+                        neighbors.append((i+1, j))
         return res
         
 # @lc code=end
