bbc2f94dad7243996392d2d04f03ba28b6ea4b4d
Xinlou Lyu <lvxl595@gmail.com>
2024-01-17 14:47:15 -0500

    feat(417): python second attempt, dfs

--- python3/medium/417.pacific-atlantic-water-flow.py
+++ python3/medium/417.pacific-atlantic-water-flow.py
@@ -7,35 +7,43 @@
 # @lc code=start
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        res = []
-        if not heights or not heights[0]:
-            return res
-        num_rows, num_cols = len(heights), len(heights[0])
-        pacific, atlantic = [], []
-        for i in range(num_rows):
-            pacific.append((i, 0))
-            atlantic.append((i, num_cols - 1))
-        for j in range(num_cols):
-            pacific.append((0, j))
-            atlantic.append((num_rows - 1, j))
+        # Check if input is empty
+        if not heights or not heights[0]: 
+            return []
         
-        def bfs(queue: List[int])-> set:
-            reachable = set()
-            while queue:
-                (row, col) = queue.pop(0)
-                reachable.add((row, col))
-                for (i, j) in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-                    new_row, new_col = row + i, col + j
-                    if new_row < 0 or new_row >= num_rows or new_col < 0 or new_col >= num_cols:
-                        continue
-                    if (new_row, new_col) in reachable:
-                        continue
-                    if heights[new_row][new_col] >= heights[row][col]:
-                        queue.append((new_row, new_col))
-            return reachable
-        pac_reachable = bfs(pacific)
-        atl_reachable = bfs(atlantic)
-        return list(pac_reachable.intersection(atl_reachable))
+        # Initialize variables, including sets used to keep track of visited cells
+        num_rows, num_cols = len(heights), len(heights[0])
+        pacific_reachable = set()
+        atlantic_reachable = set()
+        
+        def dfs(row, col, reachable):
+            # This cell is reachable, so mark it
+            reachable.add((row, col))
+            for (x, y) in [(1, 0), (0, 1), (-1, 0), (0, -1)]: # Check all 4 directions
+                new_row, new_col = row + x, col + y
+                # Check if the new cell is within bounds
+                if new_row < 0 or new_row >= num_rows or new_col < 0 or new_col >= num_cols:
+                    continue
+                # Check that the new cell hasn't already been visited
+                if (new_row, new_col) in reachable:
+                    continue
+                # Check that the new cell has a higher or equal height,
+                # So that water can flow from the new cell to the old cell
+                if heights[new_row][new_col] < heights[row][col]:
+                    continue
+                # If we've gotten this far, that means the new cell is reachable
+                dfs(new_row, new_col, reachable)
+        
+        # Loop through each cell adjacent to the oceans and start a DFS
+        for i in range(num_rows):
+            dfs(i, 0, pacific_reachable)
+            dfs(i, num_cols - 1, atlantic_reachable)
+        for i in range(num_cols):
+            dfs(0, i, pacific_reachable)
+            dfs(num_rows - 1, i, atlantic_reachable)
+        
+        # Find all cells that can reach both oceans, and convert to list
+        return list(pacific_reachable.intersection(atlantic_reachable))
         
         
 # @lc code=end
