200f37aafdefa0758addde3f03b5a31e9ee63445
Xinlou Lyu <lvxl595@gmail.com>
2024-01-17 21:08:53 -0500

    feat(207): python second attmept, dfs

--- python3/medium/207.course-schedule.py
+++ python3/medium/207.course-schedule.py
@@ -6,30 +6,34 @@
 
 # @lc code=start
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        if not prerequisites:
+    def dfs(self, node, adj, visit, inStack):
+        # If the node is already in the stack, we have a cycle.
+        if inStack[node]:
             return True
-        tree, indegree = dict(), [0] * numCourses
-        for (cur, pre) in prerequisites:
-            if pre in tree:
-                tree[pre].append(cur)
-            else:
-                tree[pre] = [cur]
-            indegree[cur] += 1
-        roots = []
-        for i in range(len(indegree)):
-            if indegree[i]: continue
-            roots.append(i)
-        while roots:
-            root = roots.pop(0)
-            if root not in tree:
-                continue
-            children = tree.pop(root)
-            for child in children:
-                indegree[child] -= 1
-                if indegree[child] == 0:
-                    roots.append(child)
-        return len(tree) == 0
+        if visit[node]:
+            return False
+        # Mark the current node as visited and part of current recursion stack.
+        visit[node] = True
+        inStack[node] = True
+        for neighbor in adj[node]:
+            if self.dfs(neighbor, adj, visit, inStack):
+                return True
+        # Remove the node from the stack.
+        inStack[node] = False
+        return False
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for _ in range(numCourses)]
+        for prerequisite in prerequisites:
+            adj[prerequisite[1]].append(prerequisite[0])
+
+        visit = [False] * numCourses
+        inStack = [False] * numCourses
+        for i in range(numCourses):
+            if self.dfs(i, adj, visit, inStack):
+                return False
+        return True
+
 
 # @lc code=end
 
