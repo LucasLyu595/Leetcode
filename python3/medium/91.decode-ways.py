bd690646bf9cd19a51b0a5e966ae6dc343124d53
Xinlou Lyu <lvxl595@gmail.com>
2024-01-25 17:30:35 -0500

    feat(91): python second attempt, dp with space-optimization

--- python3/medium/91.decode-ways.py
+++ python3/medium/91.decode-ways.py
@@ -6,28 +6,19 @@
 
 # @lc code=start
 class Solution:
-    @lru_cache(maxsize=None)
-    def recursiveWithMemo(self, index, s) -> int:
-        # If you reach the end of the string
-        # Return 1 for success.
-        if index == len(s):
-            return 1
-
-        # If the string starts with a zero, it can't be decoded
-        if s[index] == '0':
-            return 0
-
-        if index == len(s)-1:
-            return 1
-        
-        answer = self.recursiveWithMemo(index + 1, s)
-        if int(s[index : index + 2]) <= 26:
-            answer += self.recursiveWithMemo(index + 2, s)
-
-        return answer
-
     def numDecodings(self, s: str) -> int:
-        return self.recursiveWithMemo(0, s)
+        if s[0] == '0': return 0
+        one = two = 1
+        for i in range(1, len(s)):
+            current = 0
+            if s[i] != '0':
+                current = one
+            two_digit = int(s[i-1: i+1])
+            if 10 <= two_digit <=26:
+                current += two
+            two = one
+            one = current
+        return one
     
 # @lc code=end
 
