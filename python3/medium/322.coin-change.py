7b0c11cc1938b232b182d55a13198452f8852991
Xinlou Lyu <lvxl595@gmail.com>
2024-01-28 16:18:06 -0500

    feat(322): python third attempt, dp bottom up

--- python3/medium/322.coin-change.py
+++ python3/medium/322.coin-change.py
@@ -7,22 +7,16 @@
 # @lc code=start
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        min_g = amount // min(coins) + 1
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
 
-        @lru_cache(maxsize=None)
-        def dfs(amount: int) -> int:
-            if amount < 0:
-                return -1
-            if not amount:
-                return 0
-            tmp = min_g
-            for coin in coins:
-                res = dfs(amount - coin)
-                if res != -1:
-                    tmp = min(res+1, tmp)
-            return tmp if tmp != min_g else -1
-        
-        return dfs(amount)
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+        return dp[amount] if dp[amount] != float('inf') else -1
+    
+# Your runtime beats 86.47 % of python3 submissions
+# Your memory usage beats 79.85 % of python3 submissions (16.9 MB)
         
             
         
