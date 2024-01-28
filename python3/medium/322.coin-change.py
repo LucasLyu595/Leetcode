2e25e427dec9d3c046e6b97a663d3631a3ceeaec
Xinlou Lyu <lvxl595@gmail.com>
2024-01-28 16:13:06 -0500

    feat(322): python second attempt, dp top down

--- python3/medium/322.coin-change.py
+++ python3/medium/322.coin-change.py
@@ -7,27 +7,24 @@
 # @lc code=start
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-        one = [0]
-        two = []
-        res = 0
-        visited = [False] * (amount+1)
-        visited[0] = True
-        while one:
-            res += 1
-            for v in one:
-                for coin in coins:
-                    tmp = v + coin
-                    if tmp == amount:
-                        return res
-                    elif tmp > amount:
-                        continue
-                    elif not visited[tmp]:
-                        visited[tmp] = True
-                        two.append(tmp)
-            one, two = two, []
-        return -1
+        min_g = amount // min(coins) + 1
+
+        @lru_cache(maxsize=None)
+        def dfs(amount: int) -> int:
+            if amount < 0:
+                return -1
+            if not amount:
+                return 0
+            tmp = min_g
+            for coin in coins:
+                res = dfs(amount - coin)
+                if res != -1:
+                    tmp = min(res+1, tmp)
+            return tmp if tmp != min_g else -1
+        
+        return dfs(amount)
+        
+            
         
 # @lc code=end
 
