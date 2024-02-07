1df3497848373b936c242b2fbb4b1f26850508ae
Xinlou Lyu <lvxl595@gmail.com>
2024-02-07 17:29:30 -0500

    feat(152): python second attempt, dp

--- python3/medium/152.maximum-product-subarray.py
+++ python3/medium/152.maximum-product-subarray.py
@@ -7,33 +7,23 @@
 # @lc code=start
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        n = len(nums)
-        res = nums[0]
-        if n == 1:
-            return res
-        def getProduct(i:int, j:int) -> int:
-            res = 1
-            for x in range(i, j+1):
-                res *= nums[x]
-            return res
-        def maxProductPartial(i: int, j: int) -> int:
-            if i == j:
-                return nums[i]
-            try:
-                index = i+nums[i:j+1].index(0)
-                tmp = [0]
-                if i <= index-1:
-                    tmp.append(maxProductPartial(i, index-1))
-                if index+1 <= j:
-                    tmp.append(maxProductPartial(index+1, j))
-                return max(tmp)
-            except ValueError:
-                negative_indices = [index for index, value in enumerate(nums[i:j+1]) if value < 0]
-                if len(negative_indices) % 2 == 0:
-                    return getProduct(i, j)
-                else:
-                    return max(getProduct(i, i+negative_indices[-1]-1), getProduct(i+negative_indices[0]+1, j))
-        return maxProductPartial(0, n - 1)
+        if len(nums) == 0:
+            return 0
+
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+
+            max_so_far = temp_max
+
+            result = max(max_so_far, result)
+
+        return result
 
 
         
