df8753d30c91ac2cac1db521c605a12434db2095
Xinlou Lyu <lvxl595@gmail.com>
2024-02-07 17:36:55 -0500

    feat(152): python third attempt, Kadane's algorithm

--- python3/medium/152.maximum-product-subarray.py
+++ python3/medium/152.maximum-product-subarray.py
@@ -7,23 +7,11 @@
 # @lc code=start
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-
-        max_so_far = nums[0]
-        min_so_far = nums[0]
-        result = max_so_far
-
+        revs = nums[::-1]
         for i in range(1, len(nums)):
-            curr = nums[i]
-            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
-            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
-
-            max_so_far = temp_max
-
-            result = max(max_so_far, result)
-
-        return result
+            nums[i] *= nums[i - 1] or 1
+            revs[i] *= revs[i - 1] or 1
+        return max(nums + revs)
 
 
         
