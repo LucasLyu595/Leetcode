39acbfbd37b08a97be38842e36524aeee3d0b56e
Xinlou Lyu <lvxl595@gmail.com>
2024-02-09 14:13:43 -0500

    feat(300): python second attempt, build subsequence

--- python3/medium/300.longest-increasing-subsequence.py
+++ python3/medium/300.longest-increasing-subsequence.py
@@ -7,15 +7,22 @@
 # @lc code=start
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n = len(nums)
-        dp = [1] * n
-        for i in range (1, n):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    dp[i] = max(dp[j]+1, dp[i])
-        return max(dp)
+        sub = [nums[0]]
+        
+        for num in nums[1:]:
+            if num > sub[-1]:
+                sub.append(num)
+            else:
+                # Find the first element in sub that is greater than or equal to num
+                i = 0
+                while num > sub[i]:
+                    i += 1
+                sub[i] = num
+
+        return len(sub)
     
-# Your runtime beats 52.55 % of python3 submissions
+
+# Your runtime beats 85.11 % of python3 submissions
 # Your memory usage beats 92.64 % of python3 submissions (16.8 MB)
             
         
