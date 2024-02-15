bffff471ebc15f10c32ed721d5c67fef67812962
Xinlou Lyu <lvxl595@gmail.com>
2024-02-15 16:12:24 -0500

    feat(62): python second attempt, recursive with memo

--- python3/medium/62.unique-paths.py
+++ python3/medium/62.unique-paths.py
@@ -6,15 +6,12 @@
 
 # @lc code=start
 class Solution:
+    @cache
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0 for _ in range(m)] for _ in range(n)]
-        dp[0] = [1 for _ in range(m)]
-        for j in range(n):
-            dp[j][0] = 1
-        for i in range(1, n):
-            for j in range(1, m):
-                dp[i][j] = dp[i][j-1] + dp[i-1][j]
-        return dp[n - 1][m - 1]
+        if m == 1 or n == 1:
+            return 1
+        
+        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
         
 # @lc code=end
 
