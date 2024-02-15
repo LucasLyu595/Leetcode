a8697cb532810a593f8d8eea6c5b199058dae0c8
Xinlou Lyu <lvxl595@gmail.com>
2024-02-15 16:14:10 -0500

    feat(62): python third attempt, math

--- python3/medium/62.unique-paths.py
+++ python3/medium/62.unique-paths.py
@@ -5,13 +5,10 @@
 #
 
 # @lc code=start
+from math import factorial
 class Solution:
-    @cache
     def uniquePaths(self, m: int, n: int) -> int:
-        if m == 1 or n == 1:
-            return 1
-        
-        return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
+        return factorial(m + n - 2) // factorial(n - 1) // factorial(m - 1)
         
 # @lc code=end
 
