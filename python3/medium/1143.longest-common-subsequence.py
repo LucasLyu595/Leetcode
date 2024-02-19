812f4e29f294c36cde720cea359e3090f970fcc6
Xinlou Lyu <lvxl595@gmail.com>
2024-02-19 14:51:57 -0500

    feat(1143): python second attempt, space optimize

--- python3/medium/1143.longest-common-subsequence.py
+++ python3/medium/1143.longest-common-subsequence.py
@@ -6,15 +6,27 @@
 
 # @lc code=start
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        @lru_cache(maxsize=None)
-        def memo_dp(i: int, j: int) -> int:
-            if i == len(text1) or j == len(text2):
-                return 0
-            if text1[i] == text2[j]:
-                return 1 + memo_dp(i+1, j+1)
-            else:
-                return max(memo_dp(i+1, j), memo_dp(i, j+1))
-        return memo_dp(0,0)
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:        # If text1 doesn't reference the shortest string, swap them.
+        if len(text2) < len(text1):
+            text1, text2 = text2, text1
+        
+        
+        # The previous and current column starts with all 0's and like 
+        # before is 1 more than the length of the first word.
+        previous = [0] * (len(text1) + 1)
+        current = [0] * (len(text1) + 1)
+        
+        # Iterate up each column, starting from the last one.
+        for col in reversed(range(len(text2))):
+            for row in reversed(range(len(text1))):
+                if text2[col] == text1[row]:
+                    current[row] = 1 + previous[row + 1]
+                else:
+                    current[row] = max(previous[row], current[row + 1])
+            # The current column becomes the previous one, and vice versa.
+            previous, current = current, previous
+        
+        # The original problem's answer is in previous[0]. Return it.
+        return previous[0]
 # @lc code=end
 
