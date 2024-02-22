d4108f6fdeeac53716a9ba791fe6db66f60f2f2d
Xinlou Lyu <lvxl595@gmail.com>
2024-02-22 14:44:09 -0500

    feat(997): python second attempt, list

--- python3/easy/997.find-the-town-judge.py
+++ python3/easy/997.find-the-town-judge.py
@@ -8,16 +8,17 @@
 from collections import defaultdict
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        if 1 == n: return n
-        trustMap = defaultdict(list)
-        trustee = set()
+        if trust == []:
+            return 1 if n==1 else -1
+        trust_count = [0] * (n+1)
         for a, b in trust:
-            trustMap[b].append(a)
-            trustee.add(a)
-        for judge in trustMap:
-            if n - 1 == len(trustMap[judge]) and sum(list(trustee)) + judge == ((n+1)*n) >> 1:
-                return judge
-        return -1
+            trust_count[a] -= 1
+            trust_count[b] += 1
+        m = max(trust_count)
+        if m == n-1:
+            return trust_count.index(m)
+        else:
+            return -1
         
         
 # @lc code=end
