951eb45c2a235e01a15ebab86c9c0b5aa90fba3b
Xinlou Lyu <lvxl595@gmail.com>
2024-03-02 22:17:30 -0500

    feat(977): python second attempt, two pointers

--- python3/easy/977.squares-of-a-sorted-array.py
+++ python3/easy/977.squares-of-a-sorted-array.py
@@ -5,27 +5,18 @@
 #
 
 # @lc code=start
-import bisect
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        left = bisect.bisect_left(nums, 0)-1
-        right = bisect.bisect_right(nums, 0, left+1, n)
-        res = [0 for _ in range(left+1, right)]
-        if right < n and 0 == nums[right]:
-            right += 1
-        while left >= 0 and right < n:
-            if abs(nums[left]) < nums[right]:
-                res.append(pow(nums[left], 2))
-                left -= 1
+        l, r= 0, len(nums)-1
+        res = []
+        while l<=r:
+            if abs(nums[l]) <= abs(nums[r]):
+                res.append(nums[r]**2)
+                r-=1
             else:
-                res.append(pow(nums[right], 2))
-                right += 1
-        for i in range(left, -1, -1):
-            res.append(pow(nums[i], 2))
-        for j in range(right, n):
-            res.append(pow(nums[j], 2))
-        return res
+                res.append(nums[l]**2)
+                l+=1
+        return res[::-1]
         
 # @lc code=end
 
