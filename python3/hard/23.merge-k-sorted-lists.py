7c950863d7b4b3168feae3b494bd55028303e451
Xinlou Lyu <lvxl595@gmail.com>
2024-03-06 03:53:19 -0500

    feat(23): python second attempt, priority queue

--- python3/hard/23.merge-k-sorted-lists.py
+++ python3/hard/23.merge-k-sorted-lists.py
@@ -10,37 +10,30 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from queue import PriorityQueue
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists: return None
-
-        def merge2Lists(list1: List[Optional[ListNode]], list2: List[Optional[ListNode]]) -> Optional[ListNode]:
-            if not list1:
-                return list2
-            if not list2:
-                return list1
-            head = ListNode()
-            cur = head
-            while list1 and list2:
-                if list1.val < list2.val:
-                    cur.next = list1
-                    list1 = list1.next
-                else:
-                    cur.next = list2
-                    list2 = list2.next
-                cur = cur.next
-            cur.next = list1 if list1 else list2
-            return head.next
+        class Wrapper():
+            def __init__(self, node):
+                self.node = node
+            def __lt__(self, other):
+                return self.node.val < other.node.val
         
-        n = len(lists)
-        interval = 1
-        while interval < n:
-            for i in range(0, n - interval, interval * 2):
-                lists[i] = merge2Lists(lists[i], lists[i + interval])
-            interval *= 2
-
-        return lists[0]
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put(Wrapper(l))
+        while not q.empty():
+            node = q.get().node
+            point.next = node
+            point = point.next
+            node = node.next
+            if node:
+                q.put(Wrapper(node))
+        return head.next
 
         
 # @lc code=end
+
 
