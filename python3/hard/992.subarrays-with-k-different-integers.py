353ae6eb24127dadae28aed1feb0c7a681d3f1e5
Xinlou Lyu <lvxl595@gmail.com>
2024-03-30 02:23:54 -0400

    feat(992): python second attempt, two pointers with one pass

--- python3/hard/992.subarrays-with-k-different-integers.py
+++ python3/hard/992.subarrays-with-k-different-integers.py
@@ -7,21 +7,27 @@
 # @lc code=start
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        def subarraysWithAtMostKDistinct(nums: List[int], k: int) -> int:
-            ans = 0
-            start = 0
-            frequency = {}
-            for end in range(n):
-                frequency[nums[end]] = frequency.get(nums[end], 0) + 1
-                while k < len(frequency):
+        ans = 0
+        start = 0
+        frequency = {}
+        local = 0
+        for num in nums:
+            curFreq = frequency.get(num, 0) + 1
+            if 1 == curFreq:
+                k -= 1
+            frequency[num] = curFreq
+            if k < 0:
+                frequency.pop(nums[start]) # due to the mantaince, the frequency of nums[start] is always 1
+                start += 1
+                k += 1
+                local = 0
+            if k == 0:
+                while frequency.get(nums[start]) > 1: # mantain the status that every element only appear once 
+                    local += 1 # really hard to think about, needs a lot of reasoning
                     frequency[nums[start]] -= 1
-                    if frequency[nums[start]] == 0:
-                        frequency.pop(nums[start])
                     start += 1
-                ans += end - start + 1
-            return ans
-        return subarraysWithAtMostKDistinct(nums, k) - subarraysWithAtMostKDistinct(nums, k-1)
+                ans += local + 1
+        return ans
 
         
 # @lc code=end
