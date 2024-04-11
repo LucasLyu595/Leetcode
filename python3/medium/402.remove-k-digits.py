6b44821c366b0e816eea12117e41203f3ff655db
Xinlou Lyu <lvxl595@gmail.com>
2024-04-10 22:32:17 -0400

    feat(402): python second attempt, optimized greedy with stack

--- python3/medium/402.remove-k-digits.py
+++ python3/medium/402.remove-k-digits.py
@@ -5,24 +5,24 @@
 #
 
 # @lc code=start
-from collections import deque
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        ans = deque()
+        numStack = []
+        
+        # Construct a monotone increasing sequence of digits
         for digit in num:
-            tmp = int(digit)
-            while ans and k and tmp < ans[-1]:
-                ans.pop()
+            while k and numStack and numStack[-1] > digit:
+                numStack.pop()
                 k -= 1
-            ans.append(tmp)
-        while k and ans:
-            ans.pop()
-            k -= 1
-        while ans and not ans[0]:
-            ans.popleft()
-        if not ans:
-            return '0'
-        return ''.join(str(digit) for digit in ans)
+        
+            numStack.append(digit)
+        
+        # - Trunk the remaining K digits at the end
+        # - in the case k==0: return the entire list
+        finalStack = numStack[:-k] if k else numStack
+        
+        # trip the leading zeros
+        return "".join(finalStack).lstrip('0') or "0"
         
 
         
