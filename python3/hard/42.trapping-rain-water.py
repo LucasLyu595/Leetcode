6f214312c4cd17d973b5dc9044c74326d4a63787
Xinlou Lyu <lvxl595@gmail.com>
2024-04-12 04:39:00 -0400

    feat(42): python third attempt, two pointers

--- python3/hard/42.trapping-rain-water.py
+++ python3/hard/42.trapping-rain-water.py
@@ -7,20 +7,20 @@
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
-        ans, current = 0, 0
-        stack = []
-        n = len(height)
-        while current < n:
-            while stack and height[current] > height[stack[-1]]:
-                top = stack.pop()
-                if not stack:
-                    break
-                distance = current - stack[-1] - 1
-                bounded_height = min(height[current], height[stack[-1]]) - height[top]
-                ans += distance * bounded_height
-            stack.append(current)
-            current += 1
+        left_max, right_max = 0, 0
+        ans = 0 
+        left, right = 0, len(height) - 1
+        while left < right:
+            if height[left] < height[right]:
+                left_max = max(left_max, height[left])
+                ans += left_max - height[left]
+                left += 1
+            else:
+                right_max = max(right_max, height[right])
+                ans += right_max - height[right]
+                right -= 1
         return ans
+        
 
 
         
