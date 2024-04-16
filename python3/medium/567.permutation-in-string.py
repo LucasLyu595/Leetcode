b20a33b917985013d7535e186ae6175ef7eff122
Xinlou Lyu <lvxl595@gmail.com>
2024-04-16 17:06:55 -0400

    feat(567): python second attempt, sliding window with comparing maps

--- python3/medium/567.permutation-in-string.py
+++ python3/medium/567.permutation-in-string.py
@@ -5,35 +5,30 @@
 #
 
 # @lc code=start
-from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        map1 = Counter(s1)
-        tmp = None
-        left = 0
-        n = len(map1)
-        numUff = 0
-        for i in range(len(s2)):
-            c = s2[i]
-            if c in map1:
-                if not tmp:
-                    tmp = map1.copy()
-                    numUff = n
-                    left = i
-                tmp[c] -= 1
-                if 0 == tmp[c]:
-                    numUff -= 1
-                    if 0 == numUff:
-                        return True
-                else:
-                    while 0 > tmp[c]:
-                        tmp[s2[left]] += 1
-                        if 1 == tmp[s2[left]]:
-                            numUff += 1
-                        left += 1
-            else:
-                if tmp:
-                    tmp = None
+        n = len(s1)
+        m = len(s2)
+        start = 0
+        if n > m:
+            return False
+        
+        s1_counts = [0] * 26
+        s2_counts = [0] * 26
+        for i in range(n):
+            s1_counts[ord(s1[i]) - ord('a')] += 1
+            s2_counts[ord(s2[i]) - ord('a')] += 1
+        
+        if s1_counts == s2_counts:
+            return True
+        for j in range(n,m):            
+            s2_counts[ord(s2[j]) - ord('a')] += 1
+            s2_counts[ord(s2[start]) - ord('a')] -= 1
+            start += 1
+
+            if s1_counts == s2_counts:
+                return True
+        
         return False
 
 
