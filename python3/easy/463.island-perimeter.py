5ef4aba7ea5cf71a7e6ede6b89f07fc921010fad
Xinlou Lyu <lvxl595@gmail.com>
2024-04-17 21:15:06 -0400

    feat(463): python first attmpt, traverse

---
+++ python3/easy/463.island-perimeter.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=463 lang=python3
+#
+# [463] Island Perimeter
+#
+
+# @lc code=start
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]:
+                    if 0 == i:
+                        ans += 1
+                    elif 0 <= i - 1 and 0 == grid[i-1][j]:
+                        ans += 1
+                    if m-1 == i:
+                        ans += 1
+                    elif i + 1 <= m - 1 and 0 == grid[i+1][j]:
+                        ans += 1
+                        
+                    if 0 == j:
+                        ans += 1
+                    elif 0 <= j - 1 and 0 == grid[i][j-1]:
+                        ans += 1
+                    if n-1 == j:
+                        ans += 1
+                    elif j + 1 <= n - 1 and 0 == grid[i][j+1]:
+                        ans += 1
+        return ans
+
+        
+# @lc code=end
+
