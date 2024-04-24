3b6ad05a1afddb9a869141d4459332bafca75a5b
Xinlou Lyu <lvxl595@gmail.com>
2024-04-23 21:42:09 -0400

    feat(1137): python second attempt, better dp

--- python3/easy/1137.n-th-tribonacci-number.py
+++ python3/easy/1137.n-th-tribonacci-number.py
@@ -6,13 +6,13 @@
 
 # @lc code=start
 class Solution:
-    @cache
     def tribonacci(self, n: int) -> int:
-        if 0 == n:
-            return 0
-        if 1 == n or 2 == n:
-            return 1
-        return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+        if n < 3:
+            return 1 if n else 0
+        a, b, c = 0, 1, 1
+        for _ in range(n - 2):
+            a, b, c = b, c, a + b + c
+        return c
         
 # @lc code=end
 
