4ea3c9024a4e06e26d814f0bf7e2fd5437dc2599
Xinlou Lyu <lvxl595@gmail.com>
2024-04-28 02:49:59 -0400

    proto(834): python second attempt, topological sort

--- python3/hard/834.sum-of-distances-in-tree.py
+++ python3/hard/834.sum-of-distances-in-tree.py
@@ -6,33 +6,52 @@
 
 # @lc code=start
 from collections import defaultdict, deque
+import copy
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
         for u, v in edges:
             graph[u].append(v)
             graph[v].append(u)
-        distances = [[-1] * n for _ in range(n)]
+        distances = [[inf] * n for _ in range(n)]
+        leaves = deque()
         for i in range(n):
             distances[i][i] = 0
-        for i in range(n):
-            frontier = deque([i])
-            visited = set([i])
-            while frontier:
-                cur = frontier.popleft()
-                for neighbor in graph[cur]:
-                    if neighbor in visited:
-                        continue
-                    if -1 == distances[i][neighbor]:
-                        distance = distances[i][cur]
-                        distances[i][neighbor] = distance + 1
-                        distances[neighbor][i] = distance + 1
-                    frontier.append(neighbor)
-                    visited.add(neighbor)
+            if 1 == len(graph[i]):
+                leaves.append(i)
+        children = defaultdict(list)
+        numNode = n
+        while numNode > 2:
+            numNode -= len(leaves) 
+            tmp = deque()
+            while leaves:
+                cur = leaves.popleft()
+                parent = graph[cur][0]
+                children[parent].append(cur)
+                graph[parent].remove(cur)
+                if 1 == len(graph[parent]):
+                    tmp.append(parent)
+            leaves = tmp
+        if 2 == numNode:
+            a, b = leaves
+            distances[a][b] = distances[b][a] = 1
+        inGraph = set(leaves)
+        while numNode < n:
+            numNode += len(leaves)
+            tmp = deque()
+            while leaves:
+                cur = leaves.popleft()
+                for child in children[cur]:
+                    tmp.append(child)
+                    for node in inGraph:
+                        parentDis = distances[cur][node]
+                        distances[child][node] = parentDis + 1
+                        distances[node][child] = parentDis + 1
+                    inGraph.add(child)
+            leaves = tmp
         return [sum(i) for i in distances]
 
 
 
-        
 # @lc code=end
 
