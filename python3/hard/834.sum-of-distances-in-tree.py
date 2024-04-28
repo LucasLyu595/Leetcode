840a2c64470080a857136f96ac878c05bb3f1144
Xinlou Lyu <lvxl595@gmail.com>
2024-04-28 19:17:27 -0400

    feat(834): python third attempt, post-order and pre-order dfs

--- python3/hard/834.sum-of-distances-in-tree.py
+++ python3/hard/834.sum-of-distances-in-tree.py
@@ -5,51 +5,32 @@
 #
 
 # @lc code=start
-from collections import defaultdict, deque
-import copy
-class Solution:
-    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
+from collections import defaultdict
+class Solution(object):
+    def sumOfDistancesInTree(self, N, edges):
+        graph = defaultdict(set)
         for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-        distances = [[inf] * n for _ in range(n)]
-        leaves = deque()
-        for i in range(n):
-            distances[i][i] = 0
-            if 1 == len(graph[i]):
-                leaves.append(i)
-        children = defaultdict(list)
-        numNode = n
-        while numNode > 2:
-            numNode -= len(leaves) 
-            tmp = deque()
-            while leaves:
-                cur = leaves.popleft()
-                parent = graph[cur][0]
-                children[parent].append(cur)
-                graph[parent].remove(cur)
-                if 1 == len(graph[parent]):
-                    tmp.append(parent)
-            leaves = tmp
-        if 2 == numNode:
-            a, b = leaves
-            distances[a][b] = distances[b][a] = 1
-        inGraph = set(leaves)
-        while numNode < n:
-            numNode += len(leaves)
-            tmp = deque()
-            while leaves:
-                cur = leaves.popleft()
-                for child in children[cur]:
-                    tmp.append(child)
-                    for node in inGraph:
-                        parentDis = distances[cur][node]
-                        distances[child][node] = parentDis + 1
-                        distances[node][child] = parentDis + 1
-                    inGraph.add(child)
-            leaves = tmp
-        return [sum(i) for i in distances]
+            graph[u].add(v)
+            graph[v].add(u)
+
+        count = [1] * N
+        ans = [0] * N
+        def dfs(node = 0, parent = None):
+            for child in graph[node]:
+                if child != parent:
+                    dfs(child, node)
+                    count[node] += count[child]
+                    ans[node] += ans[child] + count[child]
+
+        def dfs2(node = 0, parent = None):
+            for child in graph[node]:
+                if child != parent:
+                    ans[child] = ans[node] - count[child] + N - count[child]
+                    dfs2(child, node)
+
+        dfs()
+        dfs2()
+        return ans
 
 
 
