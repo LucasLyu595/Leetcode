5152782dd7537a6d3c75096e2ac6620535b9cded
Xinlou Lyu <lvxl595@gmail.com>
2024-05-01 01:35:29 -0400

    feat(1885): python second attempt, two pointers

--- python3/medium/1885.count-pairs-in-two-arrays.py
+++ python3/medium/1885.count-pairs-in-two-arrays.py
@@ -5,38 +5,28 @@
 #
 
 # @lc code=start
-from collections import Counter
-
-
 class Solution:
-    def countPairs(self, nums1: List[int], nums2: List[int]) -> int:
-        diff = [u - v for u, v in zip(nums1, nums2)]
-        counter = Counter(diff)
-        m = len(counter)
-        diff = [k for k in counter]
-        diff.sort()
-        ans = 0
-        left, right = 0, m - 1
-        sum = 0
-        suffix = [0] * m
+    def countPairs(self, nums1, nums2):
+        N = len(nums1)  # nums2 is the same length
+
+        # Difference[i] stores nums1[i] - nums2[i]
+        difference = [nums1[i] - nums2[i] for i in range(N)]
+        difference.sort()
+
+        # Count the number of valid pairs
+        result = 0
+        left = 0
+        right = N - 1
         while left < right:
-            if diff[left] + diff[right] < 0:
+            # Left makes a valid pair with right
+            # Right also makes a valid pair with the indices between the pointers
+            if difference[left] + difference[right] > 0:
+                result += right - left
+                right -= 1
+            # Left and right are not a valid pair
+            else:
                 left += 1
-                continue
-            if diff[left] + diff[right] > 0:
-                ans += counter[diff[left]] * counter[diff[right]]
-            sum += counter[diff[right]]
-            suffix[right] = sum
-            right -= 1
-        while diff[left] < 0:
-            left += 1
-        for i in range(left, m):
-            cur = counter[diff[i]]
-            if diff[i] > 0:
-                ans += cur * (cur-1) // 2 
-            if i < m - 1:
-                ans += cur * suffix[i+1]
-        return ans
+        return result
 
 
 
