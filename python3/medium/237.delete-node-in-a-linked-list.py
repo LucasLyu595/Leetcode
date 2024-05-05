7ac681e06d9c2a12165ba57e5fd90ca2ed1e0aa2
Xinlou Lyu <lvxl595@gmail.com>
2024-05-05 00:23:20 -0400

    feat(237): python second attempt, remove node

--- python3/medium/237.delete-node-in-a-linked-list.py
+++ python3/medium/237.delete-node-in-a-linked-list.py
@@ -17,13 +17,10 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        prev, curr = node, node.next
-        while curr.next:
-            prev.val = curr.val
-            prev = curr
-            curr = curr.next
-        prev.val = curr.val
-        prev.next = None
+        # Overwrite data of next node on current node.
+        node.val = node.next.val
+        # Make current node point to next of next node.
+        node.next = node.next.next
         
 # @lc code=end
 
