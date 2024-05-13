14c9fd86da47c14100f2115930857bb680ee7249
Xinlou Lyu <lvxl595@gmail.com>
2024-05-13 20:56:04 +0800

    feat(861): python second attempt, greedy without modifying input

--- python3/medium/861.score-after-flipping-matrix.py
+++ python3/medium/861.score-after-flipping-matrix.py
@@ -8,20 +8,18 @@
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        for i in range(m):
-            if grid[i][0] == 0:
-                for j in range(n):
-                    grid[i][j] = 1 - grid[i][j]
-        mul, ans = 0, 0
-        for j in range(n-1, -1, -1):
-            tmp = 0
+        ans, mul = 0, 1
+        for j in range(n-1, 0, -1):
+            numSame = 0
             for i in range(m):
-                if 1 == grid[i][j]:
-                    tmp += 1
-            tmp = max(tmp, m-tmp)
-            ans += tmp * (1 << mul)
-            mul += 1
-        return ans
+                if grid[i][j] == grid[i][0]:
+                    numSame += 1
+            numSame = max(numSame, m - numSame)
+            ans += numSame * mul
+            mul <<= 1
+        return ans + m * mul
+
+
         
 # @lc code=end
 
