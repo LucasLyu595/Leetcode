2dc472721194646cc18e026f85ae1bf20cce8324
Xinlou Lyu <lvxl595@gmail.com>
2024-05-19 14:31:24 +0800

    feat(3068): python third attempt, one pass with optimization

--- python3/hard/3068.find-the-maximum-sum-of-node-values.py
+++ python3/hard/3068.find-the-maximum-sum-of-node-values.py
@@ -7,9 +7,8 @@
 # @lc code=start
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        minGain = float('inf')
-        minLoss = -float('inf')
-        numGain = 0
+        minGain: int = inf
+        odd = 0
         ans = 0
         for num in nums:
             after = num^k
@@ -17,12 +16,12 @@ class Solution:
             if after > num:
                 ans += after
                 minGain = min(minGain, gain)
-                numGain += 1
+                odd ^= 1
             else:
                 ans += num
-                minLoss = max(minLoss, gain)
-        if numGain & 1:
-            ans = max(ans - minGain, ans + minLoss)
+                minGain = min(minGain, -gain)
+        if odd:
+            ans -= minGain
         return ans
         
 # @lc code=end
