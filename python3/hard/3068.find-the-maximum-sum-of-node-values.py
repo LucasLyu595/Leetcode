9ff1f564f199c066dc9cdfd899514c254dbfa878
Xinlou Lyu <lvxl595@gmail.com>
2024-05-19 14:20:43 +0800

    feat(3068): python second attempt, one pass

--- python3/hard/3068.find-the-maximum-sum-of-node-values.py
+++ python3/hard/3068.find-the-maximum-sum-of-node-values.py
@@ -5,27 +5,25 @@
 #
 
 # @lc code=start
-import heapq
-
-
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        diff = []
-        for i, num in enumerate(nums):
-            heapq.heappush(diff, (num - (num^k), i))
-        while len(diff) > 1:
-            left_diff, left_i = heapq.heappop(diff)
-            if left_diff > 0:
-                continue
-            right_diff, right_i = heapq.heappop(diff)
-            if left_diff + right_diff < 0:
-                nums[left_i] -= left_diff
-                nums[right_i] -= right_diff
-        return sum(nums)
-
-        
-        
-
+        minGain = float('inf')
+        minLoss = -float('inf')
+        numGain = 0
+        ans = 0
+        for num in nums:
+            after = num^k
+            gain = after - num
+            if after > num:
+                ans += after
+                minGain = min(minGain, gain)
+                numGain += 1
+            else:
+                ans += num
+                minLoss = max(minLoss, gain)
+        if numGain & 1:
+            ans = max(ans - minGain, ans + minLoss)
+        return ans
         
 # @lc code=end
 
