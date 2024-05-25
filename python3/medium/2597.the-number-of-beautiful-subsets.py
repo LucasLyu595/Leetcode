ef739c397e408667f7a95a62ab9c4ea3fd499f59
Lucas Lyu <lvxl595@gmail.com>
2024-05-25 22:32:10 +0800

    feat(2598): python forth attempt, dp with iteration

--- python3/medium/2597.the-number-of-beautiful-subsets.py
+++ python3/medium/2597.the-number-of-beautiful-subsets.py
@@ -8,37 +8,42 @@
 class Solution:
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
         total_count = 1
-        freq_map = defaultdict(lambda: defaultdict(int))
+        freq_map = defaultdict(dict)
 
         # Calculate frequencies based on remainder
-        for x in nums:
-            freq_map[x % k][x] += 1
+        for num in nums:
+            remainder = num % k
+            freq_map[remainder][num] = freq_map[remainder].get(num, 0) + 1
 
-        # Calculate subsets for each remainder group
+        # Iterate through each remainder group
         for fr in freq_map.values():
+            n = len(fr)  # Number of elements in the current group
+
             subsets = sorted(fr.items())
-            total_count *= self._count_beautiful_subsets(subsets, len(subsets), k, 0)
+            counts = [0] * (n + 1)  # Array to store counts of subsets
+            counts[n] = 1  # Initialize count for the last subset
 
-        return total_count - 1  # Subtract 1 for the empty subset
+            # Calculate counts for each subset starting from the second last
+            for i in range(n - 1, -1, -1):
 
-    def _count_beautiful_subsets(self, subsets, num_subsets, difference, i):
-        # Base case: Return 1 for a subset of size 1
-        if i == num_subsets:
-            return 1
+                # Count of subsets skipping the current subset
+                skip = counts[i + 1]
+                # Count of subsets including the current subset
+                take = 2 ** subsets[i][1] - 1
 
-        # Calculate subsets where the current subset is not taken
-        skip = self._count_beautiful_subsets(subsets, num_subsets, difference, i + 1)
+                # If next number has a 'difference', 
+                # calculate subsets; otherwise, move to next
+                if i + 1 < n and subsets[i + 1][0] - subsets[i][0] == k:
+                    take *= counts[i + 2]
+                else:
+                    take *= counts[i + 1]
 
-        # Calculate subsets where the current subset is taken
-        take = (1 << subsets[i][1]) - 1
+                # Store the total count for the current subset
+                counts[i] = skip + take
 
-        # If next number has a 'difference', calculate subsets; otherwise, move to next
-        if i + 1 < num_subsets and subsets[i + 1][0] - subsets[i][0] == difference:
-            take *= self._count_beautiful_subsets(subsets, num_subsets, difference, i + 2)
-        else:
-            take *= self._count_beautiful_subsets(subsets, num_subsets, difference, i + 1)
+            total_count *= counts[0]
 
-        return skip + take  # Return total count of subsets
+        return total_count - 1
 
         
 
