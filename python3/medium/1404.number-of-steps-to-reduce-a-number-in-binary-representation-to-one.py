cec9969ddae09a56bbaa73fc6632dff55f469c1c
Lucas Lyu <lvxl595@gmail.com>
2024-05-29 14:22:07 +0800

    feat(1404): python second attempt, convert to int

--- python3/medium/1404.number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ python3/medium/1404.number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -8,17 +8,15 @@
 class Solution:
     def numSteps(self, s: str) -> int:
         ans = 0
-        carry = 0
-        for i in range(len(s)-1, 0, -1):
-            num = int(s[i])
-            if (num + carry) & 1 == 1:
-                carry += 1
-                ans += 1
-            carry = (num + carry) >> 1
-            ans += 1
-        if carry == 1:
+        target = int(s, 2)
+        while target != 1:
+            if target & 1:
+                target += 1
+            else:
+                target >>= 1
             ans += 1
         return ans
+
 
 # @lc code=end
 
