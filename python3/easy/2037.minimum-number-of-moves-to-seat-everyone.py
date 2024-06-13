0d1d03f2534ffb84a26199d53fc915a42b625fa2
Lucas Lyu <lvxl595@gmail.com>
2024-06-13 23:29:27 +0800

    feat(2037): python second attempt, counting sort

--- python3/easy/2037.minimum-number-of-moves-to-seat-everyone.py
+++ python3/easy/2037.minimum-number-of-moves-to-seat-everyone.py
@@ -7,7 +7,19 @@
 # @lc code=start
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        return sum(abs(a - b) for a, b in zip(sorted(seats), sorted(students)))
+        max_pos = max(max(seats), max(students))
+        diffs = [0] * max_pos
+        for seat in seats:
+            diffs[seat - 1] += 1
+        for student in students:
+            diffs[student - 1] -= 1
+        carry = 0
+        ans = 0
+        for diff in diffs:
+            ans += abs(carry)
+            carry += diff
+        return ans
+
         
 # @lc code=end
 
