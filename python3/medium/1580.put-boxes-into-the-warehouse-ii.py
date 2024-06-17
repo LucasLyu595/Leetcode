b0da40a77a01656bd10261b1f4f77ed45ec4d571
Lucas Lyu <lvxl595@gmail.com>
2024-06-17 16:08:02 +0800

    feat(1580): python second attempt, greedy with sort

--- python3/medium/1580.put-boxes-into-the-warehouse-ii.py
+++ python3/medium/1580.put-boxes-into-the-warehouse-ii.py
@@ -5,46 +5,27 @@
 #
 
 # @lc code=start
-from collections import Counter
-import bisect
-
-
 class Solution:
     def maxBoxesInWarehouse(self, boxes: List[int], warehouse: List[int]) -> int:
         left, right = 0, len(warehouse) - 1
-        house = [0] * len(warehouse)
-        h = right
-        left_prev, right_prev = inf, inf
+        ans = 0
+        boxes = sorted(boxes, reverse=True)
+        b = 0
         while left <= right:
+            room = 0
             if warehouse[left] < warehouse[right]:
-                if warehouse[right] < right_prev:
-                    right_prev = warehouse[right]
-                house[h] = right_prev
+                room = warehouse[right]
                 right -= 1
             else:
-                if warehouse[left] < left_prev:
-                    left_prev = warehouse[left]
-                house[h] = left_prev
+                room = warehouse[left]
                 left += 1
-            h -= 1
-        counter = Counter(boxes)
-        mi, ma = min(boxes), max(boxes)
-        h = 0
-        ans = 0
-        usedup = False
-        for box in range(mi, ma+1):
-            while counter[box] > 0:
-                deviation = bisect.bisect_left(house[h:], box)
-                h += deviation
-                if h >= len(house):
-                    usedup = True
-                    break
-                h += 1
-                counter[box] -= 1
-                ans += 1
-            if usedup:
+            while b < len(boxes) and boxes[b] > room:
+                b += 1
+            if b == len(boxes):
                 break
-        return ans
+            b += 1
+            ans += 1
+        return an
         
         
 # @lc code=end
