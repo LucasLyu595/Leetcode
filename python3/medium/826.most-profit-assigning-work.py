aa1c0efb8e63255449fcf23e0cbfa14f4233e07b
Lucas Lyu <lvxl595@gmail.com>
2024-06-18 14:30:25 +0800

    feat(826): python first attempt, greedy with sort and counter

---
+++ python3/medium/826.most-profit-assigning-work.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=826 lang=python3
+#
+# [826] Most Profit Assigning Work
+#
+
+# @lc code=start
+from collections import Counter
+import heapq
+
+
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        counter = Counter(worker)
+        workers = sorted(counter.keys())
+        jobs = {}
+        for diff, pro in zip(difficulty, profit):
+            jobs[diff] = max(jobs.get(diff, 0), pro)
+        difficulty = sorted(jobs.keys())
+        ans = 0
+        profit = []
+        heapq.heapify(profit)
+        j = 0
+        for worker in workers:
+            while j < len(difficulty) and difficulty[j] <= worker:
+                heapq.heappush(profit, -jobs[difficulty[j]])
+                j += 1
+            if profit:
+                ans -= profit[0] * counter[worker]
+        return ans
+
+        
+# @lc code=end
+
