f8d455be0e197530a007d1d2b3098c88b8fae25d
Lucas Lyu <lvxl595@gmail.com>
2024-06-30 17:29:05 +0800

    feat(2192): python second attempt, optimized topological sort

--- python3/medium/2192.all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ python3/medium/2192.all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -12,29 +12,26 @@ class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         graph = defaultdict(list)
         in_degree = [0] * n
+        ance_sets = [set() for _ in range(n)]
         for u, v in edges:
             graph[u].append(v)
             in_degree[v] += 1
+            ance_sets[v].add(u)
         roots = deque()
         for i in range(n):
             if in_degree[i]:
                 continue
             roots.append(i)
-        ance_sets = [set() for _ in range(n)]
         while roots:
-            next = deque()
-            while roots:
-                curr = roots.popleft()
-                for child in graph[curr]:
-                    ance_sets[child].update(ance_sets[curr])
-                    ance_sets[child].add(curr)
-                    in_degree[child] -= 1
-                    if not in_degree[child]:
-                        next.append(child)
-            roots = next
+            curr = roots.popleft()
+            for child in graph[curr]:
+                ance_sets[child].update(ance_sets[curr])
+                in_degree[child] -= 1
+                if not in_degree[child]:
+                    roots.append(child)
         ans = []
         for ance in ance_sets:
-            ans.append(sorted(list(ance)))
+            ans.append(sorted(ance))
         return ans
         
 # @lc code=end
