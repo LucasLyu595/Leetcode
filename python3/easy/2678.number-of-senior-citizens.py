7cc4f281dca58e17c485bd1e65e6af769730e1f7
Lucas Lyu <lvxl595@gmail.com>
2024-08-02 01:03:16 +0800

    feat(2678): python first attempt, sum

---
+++ python3/easy/2678.number-of-senior-citizens.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=2678 lang=python3
+#
+# [2678] Number of Senior Citizens
+#
+
+# @lc code=start
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        return sum(int(s[11:13]) > 60 for s in details)
+        
+# @lc code=end
+
