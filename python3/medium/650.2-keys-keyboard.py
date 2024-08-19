cf07b373b28f875a51bd6484b1d4a395e8862b47
Lucas Lyu <lvxl595@gmail.com>
2024-08-20 01:21:02 +0800

    feat(650): python first attempt, bottom up dp

---
+++ python3/medium/650.2-keys-keyboard.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=650 lang=python3
+#
+# [650] 2 Keys Keyboard
+#
+
+# @lc code=start
+class Solution:
+    def minSteps(self, n: int) -> int:
+        dp = [1000] * (n + 1)
+
+        dp[1] = 0
+        for i in range(2, n + 1):
+            for j in range(1, i // 2 + 1):
+                if i % j == 0:
+                    dp[i] = min(dp[i], dp[j] + i // j)
+
+        return dp[n]
+
+        
+# @lc code=end
+
