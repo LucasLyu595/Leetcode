5d2f3e479f061676058452979199d1ce05ef80c7
Lucas Lyu <lvxl595@gmail.com>
2024-09-02 11:44:07 -0400

    feat(1894): python second attempt, optimized prefix sum

--- python3/medium/1894.find-the-student-that-will-replace-the-chalk.py
+++ python3/medium/1894.find-the-student-that-will-replace-the-chalk.py
@@ -12,8 +12,12 @@ class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
         prefix = [0] * len(chalk)
         prefix[0] = chalk[0]
+        if prefix[0] > k:
+            return 0
         for i in range(1, len(chalk)):
             prefix[i] = chalk[i] + prefix[i - 1]
+            if prefix[i] > k:
+                return i
         sum = prefix[-1]
         k %= sum
         return bisect.bisect_right(prefix, k)
